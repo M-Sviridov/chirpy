5824db302e0c28b5c82c51f12722973a605f5fbe
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-17 13:36:22 +0700

    Initialize repo and add basic servemux

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/M-Sviridov/chirpy
+
+go 1.25.3
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	srv := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+	srv.ListenAndServe()
+}
