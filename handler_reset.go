a7ba40ca7b51c275cdf92c014349567107315e97
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-20 09:28:03 +0700

    Refactor reset handler

--- handler_reset.go
+++ handler_reset.go
@@ -5,9 +5,15 @@ import "net/http"
 func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
 	if cfg.platform != "dev" {
 		respondWithError(w, http.StatusForbidden, "cannot run reset on non dev platform")
-	} else {
-		cfg.fileserverHits.Store(0)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Reset total hits to 0"))
+		return
 	}
+
+	cfg.fileserverHits.Store(0)
+	err := cfg.db.DeleteUsers(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't delete all users")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, "reset hits to 0 and delete all users in database")
 }
