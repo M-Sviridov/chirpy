a426c92a798384b1095a9c632a6e143559833767
M-Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2025-11-21 10:31:31 +0700

    Fix call to HashPassword by adding argon2id default params

--- internal/auth/password.go
+++ internal/auth/password.go
@@ -3,7 +3,7 @@ package auth
 import "github.com/alexedwards/argon2id"
 
 func HashPassword(password string) (string, error) {
-	hash, err := argon2id.CreateHash(password, nil)
+	hash, err := argon2id.CreateHash(password, argon2id.DefaultParams)
 	if err != nil {
 		return "", err
 	}
